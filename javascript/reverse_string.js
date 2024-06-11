97375831d72336e0c03c6476092f3af933ed0220
KevinHarbour <kharbour22@gmail.com>
2024-06-11 14:38:59 -0400

    reversed the string

--- javascript/reverse_string.js
+++ javascript/reverse_string.js
@@ -1,5 +1,5 @@
 function reverseString(str) {
-  // type your code here
+  str.split('').reverse('').join('')
 }
 
 if (require.main === module) {
@@ -17,3 +17,5 @@ module.exports = reverseString;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+
+
